98250d6132ad1407b324825e3d50e639bb75280d
Eld0w <contact.fdecan@gmail.com>
2018-06-18 01:16:02 +0200

    fix(tree): rethink everything

--- src/Tree.ts
+++ src/Tree.ts
@@ -1,5 +1,4 @@
-import { Type, identity, success, failure, mixed } from 'io-ts';
-import { isEqual, isArray, isString, isNil } from 'lodash';
+import * as t from 'io-ts';
 
 export interface Node {
   tag: 'node',
@@ -13,39 +12,13 @@ export interface Leaf {
 
 type Tree = Node | Leaf;
 
-const checkExactObjectProperties = (o: object, properties: Array<string>): Boolean =>
-  isEqual(Object.getOwnPropertyNames(o).sort(), properties.sort())
-
-const topDownThatTreeFrom = (root: mixed) => {
-  if (isNil(root) || typeof root !== 'object') return false;
-
-  if ('tag' in root && isString(root.tag)) {
-    if (root.tag === 'leaf' && checkExactObjectProperties(root, ['tag', 'value'])) {
-      return (isString(root.value));
-    } else if (root.tag === 'node' && checkExactObjectProperties(root, ['tag', 'children'])) {
-      if (isArray(root.children)) {
-        root.children.forEach(child => {
-          return topDownThatTreeFrom(child);
-        });
-        return true; // remove this if empty arrays shouldn't be accepted
-      }
-    }
-  }
-
-  return false;
-}
-
-export class TreeType extends Type<Tree> {
-  // equivalent to Type<Tree, Tree, mixed> as per type parameter defaults
-  readonly _tag: 'TreeType' = 'TreeType'
-  constructor() {
-    super(
-      'Tree',
-      (m): m is Tree => topDownThatTreeFrom(m),
-      (m, c) => (this.is(m) ? success(m) : failure(m, c)),
-      identity
-    )
-  }
-}
-
-export const Tree: Type<Tree> = new TreeType();
+export const Tree = t.recursion<Tree>('Tree', self => t.union([
+  t.exact(t.interface({
+    tag: t.literal('node'),
+    children: t.array(self)
+  })),
+  t.exact(t.interface({
+    tag: t.literal('leaf'),
+    value: t.string
+  }))])
+)
